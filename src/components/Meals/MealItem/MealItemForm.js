cc5e4532ba4000f4222e602928d513880a569e16
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-04-19 00:12:22 +0530

    Making a hard Coded Cart without any Functionality

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -4,7 +4,9 @@ import Input from '../../UI/Input'
 
 const MealItemForm = (props) => {
   return ( <form className={classes.form}>
-    <Input label="Amount" input={{
+    <Input 
+    label="Amount" 
+    input={{
         id: 'amount',
         type: 'number',
         min:'1',
